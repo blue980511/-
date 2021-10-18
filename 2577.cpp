3e85130c3dde91694d88e530c77ede1550e53a83
blue980511 <75600390+blue980511@users.noreply.github.com>
2021-10-18 11:58:12 +0900

    Update 2577.cpp

--- 2577.cpp
+++ 2577.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <string>
 using namespace std;
 int main()
 {
@@ -11,39 +10,7 @@ int main()
 	{
 		number[10] = mul % 10;
 		mul /= 10;
-		switch (number[10])
-		{
-		case 1:
-			number[1]++;
-			break;
-		case 2:
-			number[2]++;
-			break;
-		case 3:
-			number[3]++;
-			break;
-		case 4:
-			number[4]++;
-			break;
-		case 5:
-			number[5]++;
-			break;
-		case 6:
-			number[6]++;
-			break;
-		case 7:
-			number[7]++;
-			break;
-		case 8:
-			number[8]++;
-			break;
-		case 9:
-			number[9]++;
-			break;
-		default:
-			number[0]++;
-			break;
-		}
+        number[number[10]]++;
 	}
 	for (int i = 0; i < 10; i++)
 		cout << number[i] << endl;
