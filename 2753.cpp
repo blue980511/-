5bec02ef60c6c36b18f432385a96696d68e2af4d
blue980511 <75600390+blue980511@users.noreply.github.com>
2021-11-07 19:12:01 +0900

    Add files via upload

---
+++ 2753.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#define endl '\n'
+using namespace std;
+
+int main()
+{
+	int year;
+	cin >> year;
+	if ((year % 4 == 0) && (year % 100 != 0 || year % 400 == 0))
+		cout << 1;
+	else
+		cout << 0;
+}
